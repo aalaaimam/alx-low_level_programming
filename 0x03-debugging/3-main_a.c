44e774a657be375aee0dea2fe320fa1bdcf37af1
alaaimam <aalaaimam@gmail.com>
2023-07-20 13:57:59 -0700

    C - More functions, more nested loops

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -15,6 +15,6 @@ day = 01;
 year = 1997;
 printf("Date: %02d/%02d/%04d\n", month, day, year);
 day = convert_day(month, day);
-print_remining_days(month, day, year);
+print_remaning_days(month, day, year);
 return (0);
 }
