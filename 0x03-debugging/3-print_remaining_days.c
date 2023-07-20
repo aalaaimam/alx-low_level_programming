98ee3b4a5f4664d4379de0e93030ed2862c73a99
alaaimam <aalaaimam@gmail.com>
2023-07-20 16:00:52 -0700

    C - Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
-#include "main.h"
-/**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
-void print_remaining_days(int month, int day, int year)
+int main(void)
 {
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-    {
-        if (month > 2 && day == 29)
-        {
-            day++;
-        }
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month == 2 && day == 29)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
+    int year, month, day;
+    int leap_year;
+    int days_in_month;
+    int days_in_year;
+    int remaining_days;
+    printf("Enter year: ");
+    scanf(" %d ", &year);
+    printf("Enter month: ");
+    scanf(" %d ", &month);
+    printf("Enter day: ");
+    scanf(" %d ", &day);
+    leap_year = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    days_in_month = (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) ? 31 : (month == 4 || month == 6 || month == 9 || month == 11) ? 30 : leap_year ? 29 : 28;
+    days_in_year = 365 + leap_year;
+    remaining_days = days_in_year - (day + (month - 1) * days_in_month);
+    printf("There are %d days remaining in the year.\n", remaining_days);
+    return 0;
 }
