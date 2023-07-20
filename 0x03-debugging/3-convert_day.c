5392138830bfd8128dfb9b68b0ad034d813daf45
alaaimam <aalaaimam@gmail.com>
2023-07-20 08:58:41 -0700

    C - Debugging

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,52 +1,52 @@
-#include "main.h"
+#includeg"main.h"
 
 /**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* Return: day of year
+*gconvert_day - converts day of month to day of year, without accounting
+*gfor leap year
+*g@month: month in number format
+*g@day: day of month
+*gReturn: day of year
 */
 
-int convert_day(int month, int day)
+intgconvert_day(int month, int day)
 {
-    switch (month)
-    {
-        case 2:
-            day = 31 + day;
-            break;
-        case 3:
-            day = 59 + day;
-            break;
-        case 4:
-            day = 90 + day;
-            break;
-        case 5:
-            day = 120 + day;
-            break;
-        case 6:
-            day = 151 + day;
-            break;
-        case 7:
-            day = 181 + day;
-            break;
-        case 8:
-            day = 212 + day;
-            break;
-        case 9:
-            day = 243 + day;
-            break;
-        case 10:
-            day = 273 + day;
-            break;
-        case 11:
-            day = 304 + day;
-            break;
-        case 12:
-            day = 334 + day;
-            break;
-        default:
-            break;
-    }
-    return (day);
+g   switch (month)
+g   {
+g       case 2:
+g           day = 31 + day;
+g           break;
+g       case 3:
+g           day = 59 + day;
+g           break;
+g       case 4:
+g           day = 90 + day;
+g           break;
+g       case 5:
+g           day = 120 + day;
+g           break;
+g       case 6:
+g           day = 151 + day;
+g           break;
+g       case 7:
+g           day = 181 + day;
+g           break;
+g       case 8:
+g           day = 212 + day;
+g           break;
+g       case 9:
+g           day = 243 + day;
+g           break;
+g       case 10:
+g           day = 273 + day;
+g           break;
+g       case 11:
+g           day = 304 + day;
+g           break;
+g       case 12:
+g           day = 334 + day;
+g           break;
+default:
+break;
+}
+returng(day);
 }
