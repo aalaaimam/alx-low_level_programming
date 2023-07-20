f0da92a8062fa0f6028eab872e03a3c9977b3e08
alaaimam <aalaaimam@gmail.com>
2023-07-20 10:39:40 -0700

    C - Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,6 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void pint_remaning_days(int month, int day, int year);
+void print_remaning_days(int month, int day, int year);
 int convert_day(int month, int day);
-#endif
+#endif /* MAIN_H */
