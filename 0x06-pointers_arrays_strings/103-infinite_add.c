e4e0545098de815e979c0ddc7f0c7f25ba44bbaf
alaaimam <aalaaimam@gmail.com>
2023-07-27 13:55:07 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,50 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * infinite_add - Adds two numbers stored as strings
- * @n1: The first number to add
- * @n2: The second number to add
- * @r: The buffer to store the result in
- * @size_r:
- * This function adds two numbers stored as strings,
- * and stores the result in a buffer
- * provided by the caller.
- * The size of the buffer must be large enough to hold the
- * result, or the function will return NULL.
- * The function returns a pointer to the
- * result buffer on success, or NULL on failure
+ * infinite_add - adds two numbers
+ * @n1: pointer to first number
+ * @n2: pointer to second number
+ * @r: buffer where the result will be stored
+ * @size_r: max size of the buffer
+ * Return: a pointer to the result or
+ * 0 if the result can't be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1 = 0, len2 = 0, carry = 0, sum = 0;
-while (n1[len1])
-len1++;
-while (n2[len2])
-len2++;
-if (len1 >= size_r || len2 >= size_r || size_r <= 0)
-{
+int i = 0, j = 0, len = 0, k, a, b, abs = 0;
+while (n1[i] != '\0')
+i++;
+while (n2[j] != '\0')
+j++;
+if (i > j)
+len = i;
+else
+len = j;
+if ((len + 1) > size_r)
 return (0);
-}
-r[size_r - 1] = '\0';
-while (len1 > 0 || len2 > 0 || carry != 0)
+r[len] = '\0';
+for (k = len - 1; k >= 0; k--)
 {
-sum = carry;
-if (len1 > 0)
-{
-sum += n1[--len1] - '0';
+i--;
+j--;
+if (i >= 0)
+a = n1[i] - '0';
+else
+a = 0;
+if (j >= 0)
+b = n2[j] - '0';
+else
+b = 0;
+r[k] = (a + b + abs) % 10 + '0';
+abs = (a + b + abs) / 10;
 }
-if (len2 > 0)
+if (abs == 1)
 {
-sum += n2[--len2] - '0';
-}
-carry = sum / 10;
-r[--size_r] = (sum % 10) + '0';
-}
-if (r[0] == '0' && r[1])
-{
+r[len + 1] = '\0';
+if ((len + 2) > size_r)
 return (0);
+while (len-- >= 0)
+r[len + 1] = r[len];
+r[0] = abs + '0';
 }
 return (r);
 }
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,47 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_buffer - prints buffer
- * @b: buffer
- * @size: size
- * Return: void
+ * print_buffer - prints content of size bytes of buffer pointed by b
+ * @b: buffer.
+ * @size: size of buffer.
+ * The output should print 10 bytes per line
+ *	line starts with position of 1st byte of line in hex(8chars)-start with 0
+ *	line show hex content(2chars) of buff, 2 bytes per time seperated by space
+ *	line shows content of buff. If byte is a printable char print letter else .
+ *	Each line ends with a new line \n
+ *	If size is 0 or less, the output should be a new line only \n
+ * Return: void.
  */
 void print_buffer(char *b, int size)
 {
-int o, j, i;
-o = 0;
+int j, k, l;
 if (size <= 0)
-{
 printf("\n");
-return;
-}
-while (o < size)
-{
-j = size - o < 10 ? size - o : 10;
-printf("%08x: ", o);
-for (i = 0; i < 10; i++)
-{
-if (i < j)
-printf("%02x", *(b + o + i));
 else
+{
+for (j = 0; j < size; j += 10)
+{
+printf("%.8x:", j);
+for (k = j; k < j + 10; k++)
+{
+if (k % 2 == 0)
 printf(" ");
-if (i % 2)
-{
+if (k < size)
+printf("%.2x", *(b + k));
+else
+printf("  ");
+}
 printf(" ");
-}
-}
-for (i = 0; i < j; i++)
+for (l = j; l < j + 10; l++)
 {
-int c = *(b + o + i);
-if (c < 32 || c > 132)
-{
-c = '.';
-}
-printf("%c", c);
+if (l >= size)
+break;
+if (*(b + l) < 32 || *(b + l) > 126)
+printf("%c", '.');
+else
+printf("%c", *(b + l));
 }
 printf("\n");
-o += 10;
+}
 }
 }
