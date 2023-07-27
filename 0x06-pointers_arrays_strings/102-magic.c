a91ddd5d888349abed902a81eebd8b08471e3d6e
alaaimam <aalaaimam@gmail.com>
2023-07-27 16:08:47 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,7 +7,7 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &a[2]; /* point p to the address of a[2] */
-*(p + 1) = 98; /* add 1 to the address to modify the next integer value */
+*(p + 5) = 98; /* add 5 to the address to modify the next integer value */
 printf("a[2] = %d\n", a[2]);
 return (0); /* parentheses added around return value */
 }
