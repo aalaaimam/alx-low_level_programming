3c7abdda3c187aa4c9b75c9268b174e9eec2539d
alaaimam <aalaaimam@gmail.com>
2023-07-26 17:03:07 -0700

    C-Pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,3 +1,20 @@
+/*
+ * 102-magic.c - a program that demonstrates pointer
+ * arithmetic by modifying a value
+ * in an array using only a pointer and
+ * the *(dereference) operator.
+ *
+ * The program initializes an integer array and a pointer
+ * variable and then sets the
+ * third element of the array to 1024.
+ * It then modifies the value of a different integer
+ * variable using pointer arithmetic only,
+ * by adding 5 to the pointer and dereferencing
+ * the result to set the value to 98.
+ * Finally, the program prints the value of the third
+ * element of the array to confirm that it has not been modified.
+ * Usage: ./102-magic
+ */
 #include <stdio.h>
 int main(void)
 {
