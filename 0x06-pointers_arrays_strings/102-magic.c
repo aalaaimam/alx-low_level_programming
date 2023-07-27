dfe42823e714dc4b3ca0e79eb5c0fdace9711d2b
alaaimam <aalaaimam@gmail.com>
2023-07-27 15:52:07 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
+/* This program demonstrates pointer arithmetic in C */
 int main(void)
 {
 int n;
 int a[5];
 int *p;
 a[2] = 1024;
-p = &n;
- /*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
-*(p + 5) = 98;
- /* ...so that this prints 98\n */
+p = &a[2]; /* point p to the address of a[2] */
+*(p + 1) = 98; /* add 1 to the address to modify the next integer value */
 printf("a[2] = %d\n", a[2]);
-return (0);
+return (0); /* parentheses added around return value */
 }
