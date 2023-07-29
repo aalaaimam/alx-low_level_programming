1bb530d1ec1c1b73c063df1f880cf7434f52df80
alaaimam <aalaaimam@gmail.com>
2023-07-29 12:00:13 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+
 /**
  * main - Entry point for the program
  *
  * Return: 0 on success
- */
+**/
 int main(void)
 {
-int n;
-int a[5];
-int *p;
-a[2] = 1024;
-p = &n;
-/* Update integer value 5 places after the memory location of n to 98 */
-*((int *)((char *)p + 5 * sizeof(int))) = 98;
-/* Print value of a[2] (should be 98) */
-printf("a[2] = %d\n", a[2]);
-return (0);
+ int n;
+ int a[5];
+ int *p;
+ 
+ a[2] = 1024;
+ p = &n;
+ 
+ *((int *)((char *)p + 5)) = 98;
+ 
+ printf("a[2] = %d\n", a[2]);
+
+ return (0);
 }
+
