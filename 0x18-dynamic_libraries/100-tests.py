f12499c3b8ac9cb3306ba65f8936a9b7d1f2566c
aalaaimam <aalaaimam@gmail.com>
2024-04-22 13:46:34 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -1,16 +1,13 @@
+# 100-tests.py
+
 import random
 import ctypes
-import os
 
-# Get the full path to the shared object file
-so_file_path = os.path.abspath('100-operations.so')
+cops = ctypes.CDLL('./100-operations.so')
 
-# Load the shared object file
-cops = ctypes.CDLL(so_file_path)
-
-# Rest of the script remains the same
 a = random.randint(-111, 111)
 b = random.randint(-111, 111)
+
 print("{} + {} = {}".format(a, b, cops.add(a, b)))
 print("{} - {} = {}".format(a, b, cops.sub(a, b)))
 print("{} x {} = {}".format(a, b, cops.mul(a, b)))
