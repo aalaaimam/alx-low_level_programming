b03ec1aae230a254060446ecd8299b35ad92ca0d
aalaaimam <aalaaimam@gmail.com>
2024-04-22 12:48:34 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Compile each .c file into its own object file, except main.c and 101-mul.c
+# Compile each .c file into its own object file, excluding source_0.c, source_1.c, and source_2.c
 for file in *.c; do
-if [ "$file" != "0-main.c" ] && [ "$file" != "101-mul.c" ]; then
-gcc -c -Wall -Werror -fpic "$file" -o "${file%.c}.o"
-fi
+    if [ "$file" != "source_0.c" ] && [ "$file" != "source_1.c" ] && [ "$file" != "source_2.c" ]; then
+        gcc -c -Wall -Werror -fpic "$file" -o "${file%.c}.o"
+    fi
 done
 
 # Create the dynamic library
