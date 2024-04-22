75fdb32a6a4ae740b9a653b43bc641cf5003112f
aalaaimam <aalaaimam@gmail.com>
2024-04-22 12:36:30 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Compile all .c files into object files
-gcc -c -Wall -Werror -fpic *.c
+# Compile each .c file into its own object file
+for file in *.c; do
+gcc -c -Wall -Werror -fpic "$file" -o "${file%.c}.o"
+done
 
 # Create the dynamic library
 gcc -shared -o liball.so *.o
