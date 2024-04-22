9e3fb3bb4a05d46e8ddb2ac67d0bdb79f8655a9f
aalaaimam <aalaaimam@gmail.com>
2024-04-22 15:44:32 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Compile rand.c into rand.so
 gcc -shared -o rand.so -fPIC rand.c
-
-# Set LD_PRELOAD to preload rand.so
 export LD_PRELOAD=$PWD/rand.so
-
-# Verify the MD5 checksum of gm
-md5sum gm
