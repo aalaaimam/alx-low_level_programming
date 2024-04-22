e0b656d72f3d69262d477c7b9203b029161a95a6
aalaaimam <aalaaimam@gmail.com>
2024-04-22 15:06:33 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-wget -P /tmp https://github.com/aalaaimam/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-# Load the libhack.so library using LD_PRELOAD
-export LD_PRELOAD=/tmp/libhack.so
+
+# Remove the script
+rm 101-make_me_win.sh
+
+# List directory contents
+ls -la
+
+# Clear command history
+history -c
+
+# List directory contents again
+ls -la
+
+# Verify MD5 sum of the gm program
+md5sum gm
+
+# Run the gm program to trigger jackpot message
+./gm 9 8 10 24 75 9
