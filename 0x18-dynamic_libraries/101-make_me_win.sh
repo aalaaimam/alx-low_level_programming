ce2a2ac1f2f7022120850cf8afa167f00c4d9dbc
aalaaimam <aalaaimam@gmail.com>
2024-04-22 14:54:18 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -2,6 +2,3 @@
 wget -P /tmp https://github.com/aalaaimam/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 # Load the libhack.so library using LD_PRELOAD
 export LD_PRELOAD=/tmp/libhack.so
-
-# Execute the gm program with the specified numbers
-./gm 9 8 10 24 75 9
