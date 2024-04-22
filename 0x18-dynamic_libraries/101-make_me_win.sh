cf6c230b9e00856a653c089d9305d81d4dd0fd41
aalaaimam <aalaaimam@gmail.com>
2024-04-22 15:26:08 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compile rand.c into rand.so
-gcc -shared -o rand.so -fPIC rand.c
-
-# Set LD_PRELOAD to preload rand.so
-export LD_PRELOAD=$PWD/rand.so
--- 0x18-dynamic_libraries/rand.c
+++ 0x18-dynamic_libraries/rand.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 int rand(void) {
 static int numbers[] = {9, 8, 10, 24, 75, 9};
 static int index = 0;
