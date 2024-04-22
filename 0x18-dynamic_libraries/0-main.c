b67d3500fd5fa1199cb567be5046ffa1b4db8d88
aalaaimam <aalaaimam@gmail.com>
2024-04-22 12:22:04 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,6 +1,8 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h> // Include stdlib.h for EXIT_SUCCESS definition
+#include <stdlib.h>
+
+size_t _strlen(const char *str);
+
 /**
 * main - check the code
 *
@@ -8,6 +10,6 @@
 */
 int main(void)
 {
-printf("%d\n", _strlen("My Dyn Lib"));
+printf("%zu\n", _strlen("My Dyn Lib")); // Note: %zu for size_t
 return (EXIT_SUCCESS);
 }
