977316d63f32aeae2966f9a5e48d8d73e606c0bb
aalaaimam <aalaaimam@gmail.com>
2024-04-22 12:55:13 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Compile each .c file into its own object file
+# Compile each .c file into its own object file, excluding files with main, _isdigit, _strlen, and _isupper functions
 for file in *.c; do
+if ! grep -q -E "(main|_isdigit|_strlen|_isupper)" "$file"; then
 gcc -c -Wall -Werror -fpic "$file" -o "${file%.c}.o"
+fi
 done
 
 # Create the dynamic library
